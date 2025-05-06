bc954654b910f79635a152b8e4e8dcca16131568
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-05-06 21:13:34 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,9 +2,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-// import About from './components/About';
+ import About from './components/About';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
@@ -19,22 +25,40 @@ const showAlert = (message,type)=>{
 }
   const toggleMode = ()=>{
     if(mode === 'light'){
-      setMode('dark')
-      document.body.style.backgroundColor = '#042743'
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled","sucess");
+      document.title = 'TextUtils - Dark Mode';
     }
     else{
-      setMode('light')
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("light mode has been enabled","sucess");
+      document.title = 'TextUtils - Light Mode';
     }
-  }
+    }
+  
   return (
 
     <>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     <Alert alert={alert}/>
 <div className="container my-3">
-  <TextForm heading="Enter the text here"/> 
- {/* <About/> */}
-</div>
+  <Router>
+<Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          
+          <Route path="/">
+            
+  <TextForm  showAlert={showAlert} heading="Enter the text here" mode={mode}/> 
+          </Route>
+  </Routes>
+
+  </Router>
+  </div>
+
     
      </>
   );
